1dcb53344779f4f583c2c3a593b7dc6ed4c7340b
nddat1811 <nddat1811@gmail.com>
2022-02-24 19:53:36 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello Codelearn");
+    if( argc == 2 )
+    {
+      printf("Tham so duoc cung cap la: %s\n", argv[1]);
+    }
+   else if( argc > 2 )
+    {
+      printf("Tham so 1 duoc cung cap la: %s\n", argv[1]);
+      printf("Tham so 2 duoc cung cap la: %s\n", argv[2]);
+    }
+    
     return 0;
 }
