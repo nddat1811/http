de59aa2341f235c2a84c26810fb47af9eb4b92fd
nddat1811 <nddat1811@gmail.com>
2022-02-25 08:59:30 +0700

    ok

--- src/t.c
+++ src/t.c
@@ -1,16 +1,9 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 
-#ifdef _WIN32
-    #include <windows.h>
-	#include <winsock2.h>
-	#include <ws2tcpip.h>
-	#include <stdio.h>
-	#pragma comment(lib, "Ws2_32.lib")
-#elif _LINUX
-	#include <sys/socket.h>
 
 
 HINSTANCE hInst;
